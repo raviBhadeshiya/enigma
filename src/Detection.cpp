37156a4f83a649c75d4fc43217db8c2ac0faada8
Ravi Bhadeshiya <rnbhadeshiya@gmail.com>
2017-12-05 13:40:00 -0500

    [ID:5.1,5.2][BASE] Class stub implemented and OpenCv working Enigma and Detection class added. CMakeList/package.xml modified for OpenCv Launch file modified for enigma node launch.

---
+++ src/Detection.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file Detection.cpp
+ * @author     Ravi Bhadeshiya
+ * @version    1.0
+ * @brief      Threat Detection Class
+ *
+ * @copyright  MIT License (c) 2017 Ravi Bhadeshiya
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "enigma/Detection.hpp"
+
+Detection::Detection() {}
+
+Detection::Detection(ros::NodeHandle n) {
+    n_ = n;
+    cv::Mat test;
+    ROS_INFO("Detection init successfully..");
+}
+
+Detection::~Detection() {}
